53a2531678f9d743e27f67b2688daf49cc9fa322
Hadi Noureddine <hadinour@umich.edu>
2022-04-14 11:14:50 -0400

    master

--- testSelectionSort.java
+++ testSelectionSort.java
@@ -17,7 +17,7 @@ testDuplicates();
 }
     public testSelectionSort() {
     }
-    
+
     public void testPositive(){
         int[] arr = new int[5];
         arr[0] = 8;
@@ -25,85 +25,85 @@ testDuplicates();
         arr[2] = 7;
         arr[3] = 10;
         arr[4] = 2;
-        
-        
+
+
         SelectionSort temp1 = new SelectionSort();
     temp1.basicSelectionSort(arr);
-        
+
       // System.out.println(arr[0]);
       // System.out.println(arr[1]);
       // System.out.println(arr[2]);
       // System.out.println(arr[3]);
       // System.out.println(arr[4]);
-        
+
         int[] Sortedarr = new int[5];
         Sortedarr[0] = 2;
         Sortedarr[1] = 7;
         Sortedarr[2] = 8;
         Sortedarr[3] = 9;
         Sortedarr[4] = 10;
-        
+
         /** add tests to check for this unit test **/
     }
-    
+
     public void testNegative(){
-        
+
     	int[] arr = new int[5];
         arr[0] = -8;
         arr[1] = -9;
         arr[2] = -7;
         arr[3] = -10;
         arr[4] = -2;
-        
-        
+
+
         SelectionSort temp1 = new SelectionSort();
     temp1.basicSelectionSort(arr);
-        
+
        //System.out.println(arr[0]);
        //System.out.println(arr[1]);
       // System.out.println(arr[2]);
        //System.out.println(arr[3]);
        //System.out.println(arr[4]);
     }
-    
+
     public void testMixed(){
-        
+
     	int[] arr = new int[5];
         arr[0] = 8;
         arr[1] = -9;
         arr[2] = -7;
         arr[3] = 0;
         arr[4] = 2;
-        
-        
+
+
         SelectionSort temp1 = new SelectionSort();
     temp1.basicSelectionSort(arr);
-       
+
        //System.out.println(arr[0]);
        //System.out.println(arr[1]);
        //System.out.println(arr[2]);
        //System.out.println(arr[3]);
        //System.out.println(arr[4]);
     }
-    
+
     public void testDuplicates(){
-        
+
     	int[] arr = new int[5];
         arr[0] = -8;
         arr[1] = 2;
         arr[2] = -8;
         arr[3] = 10;
         arr[4] = 2;
-        
-        
+
+
         SelectionSort temp1 = new SelectionSort();
     temp1.basicSelectionSort(arr);
-        
+
        System.out.println(arr[0]);
        System.out.println(arr[1]);
        System.out.println(arr[2]);
        System.out.println(arr[3]);
        System.out.println(arr[4]);
-       
+
     }
 }
